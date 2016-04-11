53dc574e8212f0f23ed8894dc801160f634e3d24
Andrew Breksa <abreksa4@gmail.com>
2016-04-11 16:07:25 -0700

    removed makedocs.cmd

--- makedocs.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-del ..\mini-config-docs\* /Q
-rmdir ..\mini-config-docs\resources /S /Q
-php apigen.phar generate -s src -d ..\mini-config-docs --title="mini-config" --tree --base-url "http://abreksa4.github.io/mini-config-docs/"
